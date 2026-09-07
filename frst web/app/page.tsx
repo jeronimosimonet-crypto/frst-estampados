3ccc2b13359c15d400d240652769fec1638d353c
jeronimosimonet-crypto <jeronimosimonet@gmail.com>
2026-09-07 10:50:43 -0300

    Delete frst web directory

--- frst web/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import DtfApp from '@/components/dtf-app';
-import { requireAdmin } from '@/app/admin-auth';
-
-export const dynamic = 'force-dynamic';
-
-export default async function HomePage() {
-  await requireAdmin('/');
-  return <DtfApp userName="Jeronimo Simonet" />;
-}
