391563e560914ae137c238f947743628f4baa584
jeronimosimonet-crypto <jeronimosimonet@gmail.com>
2026-09-07 10:22:06 -0300

    Delete FRST-Estampados-codigo-completo directory

--- FRST-Estampados-codigo-completo/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import DtfApp from '@/components/dtf-app';
-import { requireChatGPTUser } from '@/app/chatgpt-auth';
-
-export const dynamic = 'force-dynamic';
-
-export default async function HomePage() {
-  await requireChatGPTUser('/');
-  return <DtfApp userName="Jeronimo Simonet" />;
-}
